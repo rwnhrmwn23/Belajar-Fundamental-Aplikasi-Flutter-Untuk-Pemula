8ff697229ae55294acd4b716838ee797eb517075
yogawicak <rwnhermawan@gmail.com>
2021-10-11 14:13:32 +0700

    update flutter with StatelessWidget and StatefulWidget

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -12,14 +12,56 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.red),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Hello Flutter'),
-        ),
-        body: const Center(
-          child: Text('My First Flutter Project'),
+      home: const Scaffold(
+        body: Center(
+          child: BiggerText(text: "Hello World!")
         ),
       ),
+    );
+  }
+}
+
+class Heading extends StatelessWidget {
+  final String text;
+
+  const Heading({Key? key, required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: const TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+    );
+  }
+}
+
+class BiggerText extends StatefulWidget {
+  final String text;
+
+  const BiggerText({required this.text});
+
+  @override
+  _BiggerTextState createState() => _BiggerTextState();
+}
+
+class _BiggerTextState extends State<BiggerText> {
+  double _textSize = 16.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(widget.text, style: TextStyle(fontSize: _textSize)),
+        ElevatedButton(
+          child: const Text("Perbesar"),
+          onPressed: () {
+            setState(() {
+              _textSize = 32.0;
+            });
+          },
+        ),
+      ],
     );
   }
 }
