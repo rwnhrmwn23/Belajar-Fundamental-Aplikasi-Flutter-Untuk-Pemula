454baf3b84338eaf59cda180f7fa68f9ee76aed1
yogawicak <rwnhermawan@gmail.com>
2021-10-14 18:38:24 +0700

    update submission

--- submission/lib/main.dart
+++ submission/lib/main.dart
@@ -6,24 +6,38 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: DetailScreen()
-    );
+        title: "Pet Kita",
+        theme: ThemeData(),
+        home: DetailScreen());
   }
 }
 
 class DetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(178, 224, 218, 100),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: const [
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: Text(
+              "I love playing with toys, going for walks and getting belly rubs.",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Color.fromRGBO(71, 69, 109, 100),
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
-
 }
